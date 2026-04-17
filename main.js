c449aa54a7b3d3c4f0a4e651f23daa7d5eadfc59
robles-9r-ay2526 <robles.db@obmontessori.edu.ph>
2026-04-17 12:56:17 +0800

    AA

---
+++ main.js
@@ -0,0 +1,11 @@
+Function OPENUP(){
+    let carCounter = 18;
+    const maxCars = 25;
+
+    while(carCounter < maxCars) {
+        window.alert( "The Car" + carCounter + "has entered");
+        carCounter += 1;
+    }
+    
+    Window.alert("Fullparking");
+} 
